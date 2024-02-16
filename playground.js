0c2941e4b3e2fb5a140f050c1664f40ef0d6bb01
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-02-16 17:23:05 +0600

    alive liveScore and aliveLife

--- playground.js
+++ playground.js
@@ -1,18 +1,66 @@
 const keys = 'abcdefghijklmnopqrstuvwxyz';
 const keysArray = keys.split('');
 
-function setBackgroundColor(elementId){
+function setBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+function removeBackgroundColor(e) {
+    const key = document.getElementById(e);
+    key.classList.remove('bg-orange-400');
+}
 
-function continueGame (){
-    const randomKey = Math.random()*25
+
+function handleKeyboardKeyUpPress(event) {
+    const userPressed = event.key;
+    const currentLetter = document.getElementById('display-key').innerText.toLowerCase();
+    if (userPressed === currentLetter) {
+        removeBackgroundColor(currentLetter);
+        continueGame();
+        const liveScore = getTextElementValueById('current-score');
+        const updateValue = liveScore + 1;
+        updateTextValue('current-score', updateValue);
+        // ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+        // let currentScore = document.getElementById('current-score');
+        // const currentP =currentScore.innerText;
+        // const liveScore = parseInt(currentP);
+        // currentScore.innerText = liveScore + 1;
+        // console.log(liveScore);
+    }
+    else {
+        const aliveLife = getTextElementValueById('current-life');
+        const updateLife = aliveLife - 1;
+        updateTextValue('current-life', updateLife)
+
+        // .....................................//
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLife = currentLifeElement.innerText;
+        // const aliveLife = parseInt(currentLife);
+        // currentLifeElement.innerText = aliveLife - 1;
+    };
+}
+
+function getTextElementValueById(elementId) {
+    const liveScoreElement = document.getElementById(elementId);
+    const liveScoreValue = liveScoreElement.innerText;
+    const value = parseInt(liveScoreValue);
+    return value;
+}
+function updateTextValue(elementId, value) {
+    const currentValue = document.getElementById(elementId);
+    currentValue.innerText = value;
+
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpPress);
+
+function continueGame() {
+    const randomKey = Math.random() * 25
     const key = Math.round(randomKey);
     const index = keysArray[key];
     const displayKey = document.getElementById('display-key');
     displayKey.innerText = index;
 
     setBackgroundColor(index)
-    
+
 }
